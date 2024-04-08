f84ae1af8d103cb032877dcd728edcf51fd2e387
chirag <126777836+chirag713@users.noreply.github.com>
2024-04-08 14:25:13 +0530

    changes done

---
+++ middleware.js
@@ -0,0 +1,56 @@
+
+
+import { NextResponse } from 'next/server';
+import { NextRequest } from 'next/server';
+// import { toast } from 'react-toastify';
+
+export function middleware(request) {
+    console.log("middleware executed");
+    // Example response
+
+    let token = request.cookies.get("authToken")?.value;
+
+    if(request.nextUrl.pathname ==="/api/login" || request.nextUrl.pathname==="/api/users"){
+        return;
+    }
+
+    const loggedinusernotaccessedpaths = request.nextUrl.pathname === "/signinpage" || request.nextUrl.pathname === "/signuppage";
+
+    if (loggedinusernotaccessedpaths) {
+        if (token) {
+            const redirectUrl = new URL("/addtask", request.nextUrl.origin);
+            return NextResponse.redirect(redirectUrl);
+        }
+    } else {
+        if (!token) {
+            if(request.nextUrl.pathname.startsWith("/api")){
+                return NextResponse.json({
+                    message:"access denied ",
+                    success:false,
+                },{
+                    status:401,
+                })
+            }
+
+            console.log(123);
+            request.warningMessage = "Login is required";
+            const redirectUrl = new URL("/signinpage", request.nextUrl.origin);
+            return NextResponse.redirect(redirectUrl);
+        }
+    }
+
+    console.log(token);
+
+    // Allow the request to proceed
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: [
+        '/addtask',
+        "/showtask",
+        "/signuppage",
+        "/signinpage",
+        "/showalltask",
+    ],
+};
